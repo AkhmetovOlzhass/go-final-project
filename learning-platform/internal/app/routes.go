bdf7962f3374611942d0a65c7c325fb200b8728a
sharn1999 <58104696+sharn1999@users.noreply.github.com>
2025-11-29 18:18:40 +0500

    delete: front

--- learning-platform/internal/app/routes.go
+++ learning-platform/internal/app/routes.go
@@ -26,7 +26,7 @@ func SetupRouter(c *Container) *gin.Engine {
 	router.Use(otelgin.Middleware("learning-platform"))
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:3001"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
